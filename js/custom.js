92e197adc00b1861cb94126662d815f0d7ace3b7
Tony Tambe <tambe257@gmail.com>
2015-12-12 14:16:43 -0500

    Added Highland Carpet Page

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
  
   $("#owl-example").owlCarousel({
  
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
       autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-      items : 1,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
  
   });
  
